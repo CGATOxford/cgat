c370483138fc5a96fc0a42edc263ba3ca71ec6aa
andreas.heger@gmail.com <andreas.heger@gmail.com>
2012-01-12 09:50:15 +0000

    added shell.log debug logging

--- Pipeline.py
+++ Pipeline.py
@@ -598,6 +598,8 @@ def run( **kwargs ):
         jt.joinFiles=False
 
         return jt
+
+    shellfile = os.path.join( os.getcwd(), "shell.log" )
     
     # run multiple jobs
     if options.get( "statements" ):
@@ -626,6 +628,11 @@ def run( **kwargs ):
             # create job script
             tmpfile = tempfile.NamedTemporaryFile( dir = os.getcwd() , delete = False )
             tmpfile.write( "#!/bin/bash\n" ) #  -l -O expand_aliases\n" )
+            tmpfile.write( 'echo "START--------------------------------" >> %s \n' % shellfile )
+            tmpfile.write( 'echo "statement=%s" >> %s\n' % (statement, shellfile) )
+            tmpfile.write( "set &>> %s\n" % shellfile)
+            tmpfile.write( "module list &>> %s\n" % shellfile )
+            tmpfile.write( 'echo "END----------------------------------" >> %s \n' % shellfile )
             tmpfile.write( expandStatement(statement) + "\n" )
             tmpfile.close()
 
@@ -678,6 +685,12 @@ def run( **kwargs ):
 
         tmpfile = tempfile.NamedTemporaryFile( dir = os.getcwd() , delete = False )
         tmpfile.write( "#!/bin/bash\n" ) #  -l -O expand_aliases\n" )
+
+        tmpfile.write( 'echo "START--------------------------------" >> %s \n' % shellfile )
+        tmpfile.write( 'echo "statement=%s" >> %s\n' % (statement, shellfile) )
+        tmpfile.write( "set &>> %s\n" % shellfile)
+        tmpfile.write( "module list &>> %s\n" % shellfile )
+        tmpfile.write( 'echo "END----------------------------------" >> %s \n' % shellfile )
         tmpfile.write( expandStatement( statement ) + "\n" )
         tmpfile.close()
 
