62ef570016e994bf6c5f705925b8e8d0c049b6f7
andreas.heger@gmail.com <andreas.heger@gmail.com>
2012-07-12 13:13:53 +0100

    bugfixes after merge : missing gem params and remover .pyc files from repos

--- pipeline_annotations.py
+++ pipeline_annotations.py
@@ -285,11 +285,10 @@ def calculateMappability( infile, outfile ):
     '''Calculate mappability using GEM '''
     index = P.snip(infile, ".gem")
     to_cluster = True
-    window_size = PARAMS["gem_window_size"]
-    threads = PARAMS["gem_threads"]
-    mismatches = PARAMS["gem_mismatches"]
-    max_indel_length = PARAMS["gem_max_indel_length"]
-    statement = '''gem-mappability -t %(threads)s -m %(mismatches)s --max-indel-length %(max_indel_length)s -l %(window_size)s -I %(index)s -o %(outfile)s ''' % locals()
+    statement = '''gem-mappability -t %(gem_threads)s -m %(gem_mismatches)s 
+                                   --max-indel-length %(gem_max_indel_length)s 
+                                   -l %(gem_window_size)s 
+                                   -I %(index)s -o %(outfile)s ''' 
     P.run()
 
 ###################################################################
@@ -297,7 +296,7 @@ def calculateMappability( infile, outfile ):
 def countMappableBases( infile, outfile ):
     '''Count mappable bases in genome'''
     to_cluster = True
-    statement = '''cat %(infile)s | tr -cd ! | wc -c > %(outfile)s''' % locals()
+    statement = '''cat %(infile)s | tr -cd ! | wc -c > %(outfile)s''' 
     P.run()
     
 ###################################################################
@@ -320,7 +319,7 @@ def splitMappabiliyFileByContig( infile, outfile ):
     track = P.snip( os.path.basename(infile), ".mappability" )
     statement = '''mkdir contigs; 
                    csplit -k -f contigs/contig %(infile)s '/^~[a-zA-Z]/' {100000} > %(outfile)s;
-                   rm contigs/contig00;''' % locals()
+                   rm contigs/contig00;''' 
     P.run()
 
 ###################################################################
@@ -341,12 +340,12 @@ def loadMappableBasesPerContig( infile, outfile ):
     '''load count of mappable bases per contig '''
     to_cluster = True
     header = "contig,mappable_bases"
-    statement = '''cat %(infile)s | python %(scriptsdir)s/csv2db.py
+    statement = '''cat %(infile)s 
+                   | python %(scriptsdir)s/csv2db.py
                       --table=mappable_bases_per_contig
                       --header=%(header)s
                    > %(outfile)s '''
     P.run()
-
 
 ############################################################
 ############################################################
@@ -1641,7 +1640,8 @@ def buildGFFSummary( infile, outfile ):
 ##################################################################
 @transform( (buildContigBed,
              buildPromotorRegions,
-             buildTSSRegions,
+             buildTranscriptTSS,
+             buildGeneTSS,
              buildMapableRegions,
              buildGenomicContext,
              buildGenomeGCSegmentation ),
