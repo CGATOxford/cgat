16dcc8e02536aa63c681744d89ab8cde32913b54
Ian Sudbery <ian.sudbery@dpag.ox.ac.uk>
2012-10-10 10:46:07 +0100

    more PipelineGeneset fixes

--- pipeline_annotations.py
+++ pipeline_annotations.py
@@ -1372,6 +1372,7 @@ def buildGenomicFunctionalAnnotation( infiles, outfiles ):
 
     territories_file = infiles[0]
     
+    dbh = connect()
 
     PipelineGeneset.buildGenomicFunctionalAnnotation( territories_file, dbh, outfiles )
 
