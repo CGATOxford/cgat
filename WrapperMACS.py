2be300f2a5bcf5d332b1d556e6abe1f342edecc1
Steve Sansom <stephen.sansom@dpag.ox.ac.uk>
2012-12-18 20:15:03 +0000

    Add support for parsing MACS2 output.

--- WrapperMACS.py
+++ WrapperMACS.py
@@ -41,9 +41,8 @@ import collections
 
 MacsPeak = collections.namedtuple( "MacsPeak", "contig start end length summit tags pvalue fold fdr" )
 
-def iteratePeaks( infile ):
+def iterateMacsPeaks( infile ):
     '''iterate over peaks.xls file and return parsed data.
-
     The fdr is converted from percent to values between 0 and 1.
     '''
  
@@ -59,8 +58,10 @@ def iteratePeaks( infile ):
             # convert % fdr 
             data[8] = float(data[8]) / 100.0
         elif len(data) == 8:
-            # if no fdr given, set to 0
-            data.append( 0.0 )
+            ## if no fdr given, set to 0
+            # data.append( 0.0 )
+            ## Steve - I don't understand this so I'm commenting it out and raising an error
+            raise ValueError( "FDR value not set line %s" % line )
         else:
             raise ValueError( "could not parse line %s" % line )
 
@@ -82,3 +83,48 @@ def iteratePeaks( infile ):
         data[7] = float(data[7])
         
         yield MacsPeak._make( data )
+
+Macs2Peak = collections.namedtuple( "Macs2Peak", "contig start end length summit pileup pvalue fold fdr name" )
+
+def iterateMacs2Peaks( infile ):
+    '''iterate over peaks.xls file and return parsed data.
+    The fdr is converted from percent to values between 0 and 1.
+    '''
+ 
+    for line in infile:
+        if line.startswith("#"): continue
+        if line.startswith("chr\tstart"): continue
+        # skip empty lines
+        if line.startswith("\n"): continue
+
+        data = line[:-1].split("\t")
+
+        if len(data) == 10:
+            # convert % fdr 
+            data[8] = 10**-float(data[8])
+        elif len(data) == 8:
+            # if no fdr given, set to 0
+            #data.append( 0.0 )
+            raise ValueError( "No FDR found line %s" % line )
+        else:
+            raise ValueError( "could not parse line %s" % line )
+
+        # these are 1-based coordinates
+        # macs can have negative start coordinates
+        # start
+        data[1] = max(int(data[1]) - 1, 0)
+        # end
+        data[2] = int(data[2])
+        # length
+        data[3] = int(data[3])
+        # summit
+        data[4] = int(data[4])
+        # ntags
+        data[5] = float(data[5])
+        # -10log10(pvalue)
+        data[6] = float(data[6])
+        # fold
+        data[7] = float(data[7])
+        
+        yield Macs2Peak._make( data )
+
