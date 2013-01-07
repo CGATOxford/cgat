8ee527e4e46d55dbe392636669d38d8ab9780dcb
andreas.heger@gmail.com <andreas.heger@gmail.com>
2013-01-07 11:32:10 +0000

    bam2stats.py - correct counting in gsnap files, changed output parameters

--- bam2stats.py
+++ bam2stats.py
@@ -40,9 +40,9 @@ This script takes a bam file as input and computes a few metrics:
 +------------------------+----------------------------------------------------------------+
 |total                   |total number of alignments in bam file                          |
 +------------------------+----------------------------------------------------------------+
-|mapped                  |alignments mapped to a chromosome (bam flag)                    |
+|alignments_mapped       |alignments mapped to a chromosome (bam flag)                    |
 +------------------------+----------------------------------------------------------------+
-|unmapped                |alignments unmapped (bam flag)                                  |
+|alignments_unmapped     |alignments unmapped (bam flag)                                  |
 +------------------------+----------------------------------------------------------------+
 |qc_fail                 |alignments failing QC (bam flag)                                |
 +------------------------+----------------------------------------------------------------+
@@ -66,14 +66,14 @@ This script takes a bam file as input and computes a few metrics:
 +------------------------+----------------------------------------------------------------+
 |secondary               |alignment is not primary alignment                              |
 +------------------------+----------------------------------------------------------------+
-|rna                     |          alignments mapping to regions of repetitive RNA       |
+|alignments_rna          |          alignments mapping to regions of repetitive RNA       |
 +------------------------+----------------------------------------------------------------+
-|no_rna                  |alignments mapping not to regions of repetitive RNA (if --remove|
+|alignments_no_rna       |alignments mapping not to regions of repetitive RNA (if --remove|
 |                        |-rna has been set, otherwise equal to mapped)                   |
 +------------------------+----------------------------------------------------------------+
-|duplicates              |number of alignments mapping to the same location               |
+|alignments_duplicates   |number of alignments mapping to the same location               |
 +------------------------+----------------------------------------------------------------+
-|unique                  |number of alignments mapping to unique locations                |
+|alignments_unique       |number of alignments mapping to unique locations                |
 +------------------------+----------------------------------------------------------------+
 |reads_total             |number of reads in file. Either given via --input-reads or deduc|
 |                        |ed as the sum of mappend and unmapped reads                     |
@@ -120,7 +120,19 @@ for command line help.
 Documentation
 -------------
 
-For read counts to be correct the NH flag to be set correctly.
+Reads are not counted via read name, but making use of NH and HI flags when present.
+To recap, NH is the number of reported alignments that contain the query in the current record, 
+while HI is the hit index and ranges from 0 to NH-1. 
+
+Unfortunately, not all aligners follow this convention. For example, gsnap seems to set
+NH to the number of reportable alignments, while the actual number of reported alignments in the file
+is less. Thus, if the HI flag is present, the maximum HI is used to correct the NH flag. The
+assumption is, that the same reporting threshold has been used for all alignments.
+
+If no NH flag is present, it is assumed that all reads have only been reported once.
+
+Multi-matching counts after filtering are really guesswork. Basically, the assumption is
+that filtering is consistent and will tend to remove all alignments of a query.
 
 Code
 ----
@@ -149,6 +161,41 @@ FLAGS = {
     256: 'secondary',
     512: 'qc_fail',
     1024: 'duplicate'  }
+
+def computeMappedReadsFromAlignments( total_alignments, nh, max_hi ):
+    '''compute number of reads alignment from total number of alignments'''
+    nreads_mapped = total_alignments
+    if len(nh) > 0:
+        max_nh = max(nh.keys())
+        if max_hi > 0:
+            for x in xrange( 2, min( max_nh + 1, max_hi )): 
+                nreads_mapped -= (nh[x] / x) * (x-1)
+            for x in xrange( max_hi, max_nh + 1): 
+                nreads_mapped -= (nh[x] / max_hi) * (max_hi-1)
+        else:
+            for x in xrange( 2, max(nh.keys() ) + 1 ): nreads_mapped -= (nh[x] / x) * (x-1)
+
+    return nreads_mapped
+
+def writeNH( outfile, nh, max_hi ):
+    '''output nh array, correcting for max_hi if less than nh'''
+
+    # need to remove double counting
+    # one read matching to 2 positions is only 2
+
+    max_nh = max(nh.keys())
+    if max_hi > 0:
+        for x in xrange( 1, min( max_nh + 1, max_hi) ): 
+            if nh[x] == 0: continue
+            outfile.write("%i\t%i\n" % (x, nh[x] / x))
+        for x in xrange( max_hi, max_nh + 1): 
+            if nh[x] == 0: continue
+            outfile.write("%i\t%i\n" % (x, nh[x] / max_hi))
+    else:
+        for x in xrange( 1, max_nh + 1):
+            if nh[x] == 0: continue
+            outfile.write("%i\t%i\n" % (x, nh[x] / x))
+
 
 def main( argv = None ):
     """script main.
@@ -193,51 +240,55 @@ def main( argv = None ):
 
     pysam_in = pysam.Samfile( "-", "rb" )
 
-    c, flags_counts, nh, nh_all, nm, nm_all, mapq, mapq_all = _bam2stats.count( pysam_in, options.remove_rna, rna )
+    c, flags_counts, nh_filtered, nh_all, nm_filtered, nm_all, mapq, mapq_all, max_hi = \
+        _bam2stats.count( pysam_in, options.remove_rna, rna )
 
+    if max_hi > 0 and max_hi != max( nh_all.keys() ):
+        E.warn( "max_hi(%i) is inconsistent with max_nh (%i) - counts will be corrected" \
+                    % (max_hi, max(nh_all.keys())))
     flags = sorted(flags_counts.keys())
 
     outs = options.stdout
     outs.write( "category\tcounts\tpercent\tof\n" )
-    outs.write( "total\t%i\t%5.2f\ttotal\n" % (c.input, 100.0 ) )
+    outs.write( "alignments_total\t%i\t%5.2f\ttotal\n" % (c.input, 100.0 ) )
     if c.input == 0: 
         E.warn( "no input - skipped" )
         E.Stop()
         return
 
-    nunmapped = flags_counts["unmapped"]
-    nmapped = c.input - nunmapped
-    outs.write( "mapped\t%i\t%5.2f\ttotal\n" % (nmapped, 100.0 * nmapped / c.input ) )
-    outs.write( "unmapped\t%i\t%5.2f\ttotal\n" % ( nunmapped, 100.0 * nunmapped / c.input ) )
+    nalignments_unmapped = flags_counts["unmapped"]
+    nalignments_mapped = c.input - nalignments_unmapped
+    outs.write( "alignments_mapped\t%i\t%5.2f\ttotal\n" % \
+                    (nalignments_mapped, 100.0 * nalignments_mapped / c.input ) )
+    outs.write( "alignments_unmapped\t%i\t%5.2f\ttotal\n" % \
+                    ( nalignments_unmapped, 100.0 * nalignments_unmapped / c.input ) )
 
-    if nmapped == 0: 
-        E.warn( "no mapped reads - skipped" )
+    if nalignments_mapped == 0: 
+        E.warn( "no alignments - skipped" )
         E.Stop()
         return
 
     for flag, counts in flags_counts.iteritems():
         if flag == "unmapped": continue
-        outs.write( "%s\t%i\t%5.2f\tmapped\n" % ( flag, counts, 100.0 * counts / nmapped ) )
+        outs.write( "%s\t%i\t%5.2f\talignments_mapped\n" % ( flag, counts, 100.0 * counts / nalignments_mapped ) )
 
     if options.filename_rna:
-        outs.write( "rna\t%i\t%5.2f\tmapped\n" % (c.rna, 100.0 * c.rna / nmapped ) )
-        outs.write( "no_rna\t%i\t%5.2f\tmapped\n" % (c.filtered, 100.0 * c.filtered / nmapped ) )
+        outs.write( "alignments_rna\t%i\t%5.2f\talignments_mapped\n" % (c.rna, 100.0 * c.rna / nalignments_mapped ) )
+        outs.write( "alignments_no_rna\t%i\t%5.2f\talignments_mapped\n" % (c.filtered, 100.0 * c.filtered / nalignments_mapped ) )
         normby = "norna"
     else:
         normby = "mapped"
     
     if c.filtered > 0:
-        outs.write( "duplicates\t%i\t%5.2f\t%s\n" % (c.duplicates, 100.0* c.duplicates / c.filtered, normby))
-        outs.write( "unique\t%i\t%5.2f\t%s\n" % (c.filtered - c.duplicates,
-                                                     100.0*(c.filtered - c.duplicates)/c.filtered,
-                                                       normby) )
+        outs.write( "alignments_duplicates\t%i\t%5.2f\t%s\n" % (c.duplicates, 100.0* c.duplicates / c.filtered, normby))
+        outs.write( "alignments_unique\t%i\t%5.2f\t%s\n" % (c.filtered - c.duplicates,
+                                                            100.0*(c.filtered - c.duplicates)/c.filtered,
+                                                            normby) )
 
     # derive the number of mapped reads in file from alignment counts
     nreads_unmapped = flags_counts["unmapped"]
-    nreads_mapped = nmapped
-    if len(nh_all) > 1:
-        for x in xrange( 2, max(nh_all.keys() ) + 1 ): nreads_mapped -= (nh_all[x] / x) * (x-1)
-
+    nreads_mapped = computeMappedReadsFromAlignments( nalignments_mapped, nh_all, max_hi )
+    
     nreads_missing = 0
     if options.input_reads:
         nreads_total = options.input_reads
@@ -263,16 +314,15 @@ def main( argv = None ):
     if len(nh_all) > 1:
         outs.write( "reads_unique\t%i\t%5.2f\treads_mapped\n" % (nh_all[1], 100.0 * nh_all[1] / nreads_mapped ) )
 
-    # look at various filtering options
+    # compute after filtering
+    # not that these are rough guesses
     if options.filename_rna:
-        nreads_norna = c.filtered
-        if len(nh) > 1:
-            for x in xrange( 2, max(nh.keys() ) + 1 ): nreads_norna -= (nh[x] / x) * (x-1)
+        nreads_norna = computeMappedReadsFromAlignments( c.filtered, nh_filtered, max_hi )
 
         outs.write( "reads_norna\t%i\t%5.2f\treads_mapped\n" % (nreads_norna, 100.0 * nreads_norna / nreads_mapped ) )
 
-        if len(nh) > 1:
-            outs.write( "reads_norna_unique\t%i\t%5.2f\treads_norna\n" % (nh[1], 100.0 * nh[1] / nreads_norna ) )
+        if len(nh_filtered) > 1:
+            outs.write( "reads_norna_unique\t%i\t%5.2f\treads_norna\n" % (nh_filtered[1], 100.0 * nh_filtered[1] / nreads_norna ) )
 
     pysam_in.close()
 
@@ -287,25 +337,32 @@ def main( argv = None ):
         outs.write( "pairs_total\t%i\t%5.2f\tpairs_total\n" % (pairs_total,0.0))
         outs.write( "pairs_mapped\t%i\t%5.2f\tpairs_total\n" % (pairs_mapped, 0.0))
 
-    if options.force_output or len(nm) > 0:
+    if options.force_output or len(nm_filtered) > 0:
         outfile = E.openOutputFile( "nm", "w" )
         outfile.write( "NM\talignments\n" )
-        if len(nm) > 0:
-            for x in xrange( 0, max( nm.keys() ) + 1 ): outfile.write("%i\t%i\n" % (x, nm[x]))
+        if len(nm_filtered) > 0:
+            for x in xrange( 0, max( nm_filtered.keys() ) + 1 ): outfile.write("%i\t%i\n" % (x, nm_filtered[x]))
         else:
             outfile.write( "0\t%i\n" % (c.filtered) )
         outfile.close()
 
-    if options.force_output or len(nh) > 1:
-        # need to remove double counting
-        # one read matching to 2 positions is only 2
+    if options.force_output or len(nh_all) > 1:
+        outfile = E.openOutputFile( "nh_all", "w")
+        outfile.write( "NH\treads\n" )
+        if len(nh_all) > 0:
+            writeNH( outfile, nh_all, max_hi )
+        else:
+            # assume all are unique if NH flag not set
+            outfile.write( "1\t%i\n" % (c.mapped_reads) )
+        outfile.close()
+
+    if options.force_output or len(nh_filtered) > 1:
         outfile = E.openOutputFile( "nh", "w")
         outfile.write( "NH\treads\n" )
-        if len(nh) > 0:
-            for x in xrange( 1, max( nh.keys() ) + 1 ): 
-                outfile.write("%i\t%i\n" % (x, nh[x] / x))
+        if len(nh_filtered) > 0:
+            writeNH( outfile, nh_filtered, max_hi )
         else:
-            # assume all are unique if NH flag set
+            # assume all are unique if NH flag not set
             outfile.write( "1\t%i\n" % (c.filtered) )
         outfile.close()
 
