04c35f321e9596815e0addec4b5272ac2d5392b1
Steve Sansom <stephen.sansom@dpag.ox.ac.uk>
2013-01-29 10:43:55 +0000

    minor bugfix

--- pipeline_mapping.py
+++ pipeline_mapping.py
@@ -968,7 +968,7 @@ def mapReadsWithSTAR( infile, outfile ):
                                                            PARAMS["star_memory"])
     to_cluster = True
     
-    star_mapping_genome = PARAMS["star_genome"] or PARAMS["genome"]
+    star_mapping_genome = PARAMS["star_genome"] or PARAMS["genome"]+".dir"
     
     m = PipelineMapping.STAR( executable = P.substituteParameters( **locals() )["star_executable"] )
     
