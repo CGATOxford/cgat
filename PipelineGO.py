20dd04f5f2fd99aef47295487ae67432d715d258
Steve Sansom <stephen.sansom@dpag.ox.ac.uk>
2013-03-09 17:57:57 +0000

    change so that the "go_geneontology_file" must be fully specified as the correct file to use is not predictable - e.g. there are two different files for mouse and one is 7 years old...

--- PipelineGO.py
+++ PipelineGO.py
@@ -93,7 +93,7 @@ def createGOFromGeneOntology( infile, outfile ):
     filename = "geneontology.goa.gz"
     if not os.path.exists( filename ):
         statement = '''
-    wget -O %(filename)s http://cvsweb.geneontology.org/cgi-bin/cvsweb.cgi/go/gene-associations/%(go_geneontology_file)?rev=HEAD
+    wget -O %(filename)s http://cvsweb.geneontology.org/cgi-bin/cvsweb.cgi/go/gene-associations/%(go_geneontology_file)s?rev=HEAD
     '''
     
         P.run()
