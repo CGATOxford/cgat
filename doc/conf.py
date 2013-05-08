30254bf5c134422f1a8ada47cfbbadc2c3dfc1b7
andreas.heger@gmail.com <andreas.heger@gmail.com>
2013-05-08 10:32:35 +0100

    added missing intervaldb.c

--- doc/conf.py
+++ doc/conf.py
@@ -84,7 +84,7 @@ release = '0.1'
 
 # List of directories, relative to source directory, that shouldn't be searched
 # for source files.
-exclude_trees = ['_build']
+exclude_trees = ['_build', 'obsolete']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
