1fff83f0cb22864a63984239a1cb2035cb3b13a3
katherinef <katherinef@cgat150.anat.ox.ac.uk>
2013-11-13 15:45:26 +0000

    pipeline_readqc.py - changed options for quality trimming

--- CGATPipelines/pipeline_readqc.py
+++ CGATPipelines/pipeline_readqc.py
@@ -1,3 +1,4 @@
+
 ################################################################################
 #
 #   MRC FGU Computational Genomics Group
@@ -485,7 +486,7 @@ def processReads( infiles, outfile ):
 
     # NICK - may replace fastx trimmer
     if PARAMS["process_trim_quality"]:
-        s.append( 'fastq_quality_trimmer -Q %(offset)i  -v %(trim_options)s 2>> %(outfile)s_trim.log' )
+        s.append( 'fastq_quality_trimmer -Q %(offset)i  -v %(trim_quality_options)s 2>> %(outfile)s_trim.log' )
         do_sth = True
 
     if PARAMS["process_filter"]:
