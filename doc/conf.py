8a98e34fca7020d03eaaee4819abc0083815d8be
Andreas Heger <andreas.heger@gmail.com>
2013-12-09 16:57:17 +0000

    removed SphinxReport from conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -31,10 +31,11 @@ data_dir = os.path.abspath('..')
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig',
-              'SphinxReport.only_directives', 
-	      'SphinxReport.report_directive', 
-	      'SphinxReport.roles',
+extensions = ['sphinx.ext.autodoc', 
+              'sphinx.ext.todo', 
+              'sphinx.ext.coverage', 
+              'sphinx.ext.pngmath', 
+              'sphinx.ext.ifconfig',
 	      'sphinx.ext.inheritance_diagram',
 	      'sphinxcontrib.programoutput',
 	      'sphinx.ext.intersphinx' ]
