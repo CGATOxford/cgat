608cd22e9c7b153910b0b3178bc03261d7638a2e
Andreas Heger <andreas.heger@gmail.com>
2014-02-06 15:12:16 +0000

    PipelineGeneset.py - added exon_number

--- CGATPipelines/PipelineGeneset.py
+++ CGATPipelines/PipelineGeneset.py
@@ -326,7 +326,7 @@ def loadGeneInformation( infile, outfile, only_proteincoding = False ):
     | %(filter_cmd)s 
     | python %(scriptsdir)s/gtf2gtf.py --sort=gene
     | python %(scriptsdir)s/gtf2tsv.py --full --only-attributes -v 0
-    | python %(toolsdir)s/csv_cut.py --remove exon_id transcript_id transcript_name protein_id
+    | python %(toolsdir)s/csv_cut.py --remove exon_id transcript_id transcript_name protein_id exon_number
     | %(scriptsdir)s/hsort 1 | uniq 
     | python %(scriptsdir)s/csv2db.py %(csv2db_options)s 
               --index=gene_id 
@@ -360,7 +360,7 @@ def loadTranscriptInformation( infile, outfile,
     | awk '$3 == "CDS"' 
     | python %(scriptsdir)s/gtf2gtf.py --sort=gene
     | python %(scriptsdir)s/gtf2tsv.py --full --only-attributes -v 0
-    | python %(toolsdir)s/csv_cut.py --remove exon_id 
+    | python %(toolsdir)s/csv_cut.py --remove exon_id exon_number 
     | %(scriptsdir)s/hsort 1 | uniq 
     | python %(scriptsdir)s/csv2db.py %(csv2db_options)s 
               --index=transcript_id 
