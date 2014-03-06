c2f90c34dad9f5dd4517d025dc6890215977cd7e
Andreas Heger <andreas.heger@gmail.com>
2014-03-06 14:50:33 +0000

    Masker.py - fixed: segmasker now does softmasking

--- CGAT/Masker.py
+++ CGAT/Masker.py
@@ -21,8 +21,8 @@
 #   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 ##########################################################################
 '''
-Masker.py - 
-======================================================
+Masker.py - Wrapper for sequence masking tools
+==============================================
 
 :Author: Andreas Heger
 :Release: $Id$
@@ -34,7 +34,6 @@ Code
 
 '''
 import os
-import sys
 import subprocess
 import tempfile
 import string
@@ -58,6 +57,9 @@ class Masker:
     mOptions = ""
     mHasPeptideMasking = False
     mHasNucleicAcidMasking = False
+
+    # set to true if masker outputs softmasked sequence
+    soft_mask = False
 
     def __init__(self):
         pass
@@ -88,9 +90,14 @@ class Masker:
         if len(seq) < 5:
             # do not mask empty/short sequences
             pass
+
         elif a == "aa" and self.mHasPeptideMasking:
+
             c = 0
-            for p, m in zip(sequence, self.maskSequence(sequence)):
+            m = self.maskSequence(sequence)
+            if self.soft_mask:
+                m = re.sub("[a-z]", "x", m)
+            for p, m in zip(sequence, m):
                 if m in "Xx":
                     if p.isupper():
                         seq[c] = "X"
@@ -102,6 +109,8 @@ class Masker:
 
             peptide_sequence = Genomics.TranslateDNA2Protein(sequence)
             masked_sequence = self.maskSequence(peptide_sequence)
+            if self.soft_mask:
+                masked_sequence = re.sub("[a-z]", "x", masked_sequence)
 
             c = 0
             for p, m in zip(peptide_sequence, masked_sequence):
@@ -113,7 +122,10 @@ class Masker:
                 c += 3
 
         elif a in ("na", "codons") and self.mHasNucleicAcidMasking:
-            return self.maskSequence(sequence)
+            masked_sequence = self.maskSequence(sequence)
+            if self.soft_mask:
+                masked_sequence = re.sub("[a-z]", "N", masked_sequence)
+            return masked_sequence
         else:
             raise ValueError(
                 "masking of sequence type %s not implemented." % a)
@@ -142,16 +154,15 @@ class Masker:
                              close_fds=True)
 
         (out, err) = s.communicate()
-
         if s.returncode != 0:
             raise RuntimeError(
-                "Error in running %s \n%s\nTemporary directory" % (statement, err))
+                "Error in running %s \n%s\nTemporary directory" %
+                (statement, err))
 
         os.remove(filename_peptide)
 
         masked_sequence = re.sub(
             "\s", "", string.join(out.split("\n")[1:], ""))
-
         return masked_sequence
 
     def maskSequences(self, sequences):
@@ -178,7 +189,8 @@ class Masker:
 
         if s.returncode != 0:
             raise RuntimeError(
-                "Error in running %s \n%s\nTemporary directory" % (statement, err))
+                "Error in running %s \n%s\nTemporary directory" %
+                (statement, err))
 
         result = [
             x.sequence for x in FastaIterator.iterate(StringIO.StringIO(out))]
@@ -195,23 +207,25 @@ class MaskerBias (Masker):
 
 
 class MaskerSeg (Masker):
-
     # mCommand = "seg %(infile)s 12 2.2 2.5 -x"
-    mCommand = "segmasker -in %(infile)s -window 12 -locut 2.2 -hicut 2.5 -outfmt fasta"
+    mCommand = ("segmasker -in %(infile)s -window 12 -locut 2.2 "
+                "-hicut 2.5 -outfmt fasta")
     mHasPeptideMasking = True
+    soft_mask = True
 
 
 class MaskerDustMasker(Masker):
 
-    '''use dustmasker. masked chars are returned as 
+    '''use dustmasker. masked chars are returned as
     lower case characters.'''
+
     mCommand = "dustmasker -outfmt fasta -in %(infile)s"
     mHasNucleicAcidMasking = True
 
 
 class MaskerRandom (Masker):
-
-    """randomly mask a proportion of positions in a sequence in multiple alignment."""
+    """randomly mask a proportion of positions in a sequence
+    in multiple alignment."""
 
     def __init__(self, proportion=10, *args, **kwargs):
         Masker.__init__(self, *args, **kwargs)
