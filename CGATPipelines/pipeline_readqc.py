82fde067f3c48c523d7066309e30a29944d6d925
Tom Smith <thomas.smith2@dpag.ox.ac.uk>
2014-07-11 13:23:57 +0100

    more pep8 changes

--- CGATPipelines/pipeline_readqc.py
+++ CGATPipelines/pipeline_readqc.py
@@ -55,7 +55,8 @@ Individual tasks are enabled in the configuration file.
 Usage
 =====
 
-See :ref:`PipelineSettingUp` and :ref:`PipelineRunning` on general information how to use CGAT pipelines.
+See :ref:`PipelineSettingUp` and :ref:`PipelineRunning`
+on general information how to use CGAT pipelines.
 
 Configuration
 -------------
@@ -65,46 +66,51 @@ No general configuration required.
 Input
 -----
 
-Reads are imported by placing files or linking to files in the :term:`working directory`.
+Reads are imported by placing files or linking to files in the :term:
+`working directory`.
 
 The default file format assumes the following convention:
 
    <sample>-<condition>-<replicate>.<suffix>
 
-``sample`` and ``condition`` make up an :term:`experiment`, while ``replicate`` denotes
-the :term:`replicate` within an :term:`experiment`. The ``suffix`` determines the file type.
+``sample`` and ``condition`` make up an :term:`experiment`,
+while ``replicate`` denotes the :term:`replicate` within an :term:`experiment`.
+The ``suffix`` determines the file type.
 The following suffixes/file types are possible:
 
 sra
-   Short-Read Archive format. Reads will be extracted using the :file:`fastq-dump` tool.
+   Short-Read Archive format. Reads will be extracted using the :file:
+   `fastq-dump` tool.
 
 fastq.gz
    Single-end reads in fastq format.
 
 fastq.1.gz, fastq2.2.gz
-   Paired-end reads in fastq format. The two fastq files must be sorted by read-pair.
+   Paired-end reads in fastq format.
+   The two fastq files must be sorted by read-pair.
 
 .. note::
 
-   Quality scores need to be of the same scale for all input files. Thus it might be
-   difficult to mix different formats.
+   Quality scores need to be of the same scale for all input files.
+   Thus it might be difficult to mix different formats.
 
 Requirements
 ------------
 
-On top of the default CGAT setup, the pipeline requires the following software to be in the 
-path:
+On top of the default CGAT setup, the pipeline requires the following software
+to be in the path:
 
-+--------------------+-------------------+------------------------------------------------+
-|*Program*           |*Version*          |*Purpose*                                       |
-+--------------------+-------------------+------------------------------------------------+
-|fastqc              |>=0.9.0            |read quality control                            |
-+--------------------+-------------------+------------------------------------------------+
-|sra-tools           |                   |extracting reads from .sra files                |
-+--------------------+-------------------+------------------------------------------------+
-|picard              |>=1.38             |bam/sam files. The .jar files need to be in your|
-|                    |                   | CLASSPATH environment variable.                |
-+--------------------+-------------------+------------------------------------------------+
++--------------------+-------------------+----------------------------------+
+|*Program*           |*Version*          |*Purpose*                         |
++--------------------+-------------------+----------------------------------+
+|fastqc              |>=0.9.0            |read quality control              |
++--------------------+-------------------+----------------------------------+
+|sra-tools           |                   |extracting reads from .sra files  |
++--------------------+-------------------+----------------------------------+
+|picard              |>=1.38             |bam/sam files. The .jar files need|
+|                    |                   |to be in your                     |
+|                    |                   | CLASSPATH environment variable.  |
++--------------------+-------------------+----------------------------------+
 
 Pipeline output
 ===============
@@ -225,7 +231,7 @@ transcripts = PARAMS["sailfish_transcripts"]
            REGEX_FORMATS,
            r"\1.fastqc")
 def runFastqc(infiles, outfile):
-    '''convert sra files to fastq and check mapping qualities are in solexa format. 
+    '''convert sra files to fastq and check mapping qualities are in solexa format.
     Perform quality control checks on reads from .fastq files.'''
     to_cluster = True
     m = PipelineMapping.FastQc(nogroup=PARAMS["readqc_no_group"])
@@ -432,7 +438,7 @@ def runSailfish(infiles, outfile):
 def mergeSailfishResults(infiles, outfile):
 
     statement = '''python %(scriptsdir)s/combine_tables.py
-              --glob quantification/*/*quant.sf --columns 1 --take 7 
+              --glob quantification/*/*quant.sf --columns 1 --take 7
               --use-file-prefix -v 0| gzip > %(outfile)s'''
     P.run()
 
