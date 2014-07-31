f6554f47c08aeff0f802ed6a73bf1901ce97692a
Andreas Heger <andreas.heger@gmail.com>
2014-07-31 14:40:02 +0100

    pep8 changes

--- scripts/_bam2bam.pyx
+++ scripts/_bam2bam.pyx
@@ -219,11 +219,12 @@ def filter_bam(Samfile input_samfile,
                 if nm > read_mismatches: 
                     nmismatches += 1
                     continue
-                
-        
 
         noutput += 1
-        output_samfile.write( read )
+        output_samfile.write(read)
+
+        if noutput > 100:
+            break
 
     c = E.Counter()
     c.input = ninput
@@ -235,8 +236,8 @@ def filter_bam(Samfile input_samfile,
     c.removed_mismatches = nmismatches
 
     if nremove_contig_tids:
-        free( remove_contig_tids )
+        free(remove_contig_tids)
 
-    E.info( "filtering finished" )
+    E.info("filtering finished")
 
     return c
