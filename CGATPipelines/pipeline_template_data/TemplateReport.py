f603dae4708759b2cf15bf34316562f32ca32c13
Andreas Heger <andreas.heger@gmail.com>
2014-09-18 16:32:16 +0100

    pep8 changes

--- CGATPipelines/pipeline_template_data/TemplateReport.py
+++ CGATPipelines/pipeline_template_data/TemplateReport.py
@@ -8,17 +8,19 @@ from collections import OrderedDict as odict
 ###################################################################
 ## parameterization
 
-EXPORTDIR=P['exportdir']
-DATADIR  =P['datadir']
-DATABASE =P['sql_backend']
+EXPORTDIR = P['exportdir']
+DATADIR = P['datadir']
+DATABASE = P['sql_backend']
 
 ###########################################################################
-class ProjectTracker( TrackerSQL ):
+class ProjectTracker(TrackerSQL):
     '''Define convenience tracks for plots'''
     def __init__(self, *args, **kwargs ):
         TrackerSQL.__init__(self, *args, backend = DATABASE, **kwargs )
     
-class WordFrequencies( ProjectTracker ):
+
+class WordFrequencies(ProjectTracker):
     pattern = "(.*)_counts" 
+
     def __call__(self, track ):
         return self.getValues( "SELECT freq FROM %(track)s_counts" )
