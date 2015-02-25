44b324b83850fb0e82cf31f89f369aa32abe1b25
Andreas Heger <andreas.heger@gmail.com>
2015-02-25 12:41:05 +0000

    pep8

--- CGATPipelines/pipeline_quickstart.py
+++ CGATPipelines/pipeline_quickstart.py
@@ -45,11 +45,13 @@ def main(argv=sys.argv):
     parser.add_option("-d", "--dest", dest="destination", type="string",
                       help="destination directory.")
 
-    parser.add_option("-n", "--set-name", dest="name", type="string",
-                      help="name of this pipeline. 'pipeline_' will be prefixed.")
+    parser.add_option(
+        "-n", "--set-name", dest="name", type="string",
+        help="name of this pipeline. 'pipeline_' will be prefixed.")
 
-    parser.add_option("-f", "--force-output", dest="force", action="store_true",
-                      help="overwrite existing files.")
+    parser.add_option(
+        "-f", "--force-output", dest="force", action="store_true",
+        help="overwrite existing files.")
 
     parser.set_defaults(
         destination=".",
