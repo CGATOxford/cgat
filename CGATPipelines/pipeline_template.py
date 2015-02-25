e9c87e848b22251caf6cf0a493bd32938adf42f1
Andreas Heger <andreas.heger@gmail.com>
2015-02-25 12:40:49 +0000

    simplified basic pipeline

--- CGATPipelines/pipeline_template.py
+++ CGATPipelines/pipeline_template.py
@@ -1,3 +1,25 @@
+##############################################################################
+#
+#   MRC FGU CGAT
+#
+#   $Id$
+#
+#   Copyright (C) 2009 Andreas Heger
+#
+#   This program is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License
+#   as published by the Free Software Foundation; either version 2
+#   of the License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+###############################################################################
 """===========================
 Pipeline template
 ===========================
@@ -7,10 +29,14 @@ Pipeline template
 :Date: |today|
 :Tags: Python
 
-A pipeline template.
+.. Replace the documentation below with your own description of the
+   pipeline's purpose
 
 Overview
 ========
+
+This pipeline computes the word frequencies 
+
 
 Usage
 =====
@@ -23,15 +49,12 @@ Configuration
 
 The pipeline requires a configured :file:`pipeline.ini` file.
 
-The sphinxreport report requires a :file:`conf.py` and
-:file:`sphinxreport.ini` file (see :ref:`PipelineReporting`). To start
+The CGATReport report requires a :file:`conf.py` and
+:file:`cgatreport.ini` file (see :ref:`PipelineReporting`). To start
 with, use the files supplied with the Example_ data.
 
-Input
------
-
-Optional inputs
-+++++++++++++++
+Input files
+-----------
 
 Requirements
 ------------
@@ -43,16 +66,17 @@ The pipeline requires the results from
 On top of the default CGAT setup, the pipeline requires the following
 software to be in the path:
 
-+----------+-----------+---------------------------+
-|*Program* |*Version*  |*Purpose*                  |
-+----------+-----------+---------------------------+
-|          |           |                           |
-+----------+-----------+---------------------------+
+.. Add any additional external requirements such as 3rd party software
+   or R modules below:
+
+Requirements:
+
+* samtools >= 1.1
 
 Pipeline output
 ===============
 
-The major output is in the database file :file:`csvdb`.
+.. Describe output files of the pipeline here
 
 Glossary
 ========
@@ -70,22 +94,17 @@ import sys
 import os
 import sqlite3
 import CGAT.Experiment as E
+import CGAT.Pipeline as P
 import CGAT.IOTools as IOTools
-import CGAT.Database as Database
-
-###################################################
-###################################################
-###################################################
-# Pipeline configuration
-###################################################
 
 # load options from the config file
-import CGAT.Pipeline as P
 PARAMS = P.getParameters(
     ["%s/pipeline.ini" % os.path.splitext(__file__)[0],
      "../pipeline.ini",
      "pipeline.ini"])
 
+# load configuration values from associated pipelines
+# pipeline_annotations:
 PARAMS.update(P.peekParameters(
     PARAMS["annotations_dir"],
     "pipeline_annotations.py",
@@ -94,7 +113,7 @@ PARAMS.update(P.peekParameters(
     update_interface=True))
 
 
-# Update the PARAMS dictionary in any PipelineModules
+# If necessary, update the PARAMS dictionary in any modules file.
 # e.g.:
 # import CGATPipelines.PipelineGeneset as PipelineGeneset
 # PipelineGeneset.PARAMS = PARAMS
@@ -117,14 +136,33 @@ def connect():
 
     return dbh
 
-###################################################################
-# worker tasks
-###################################################################
+
+@transform(["%s.ini" % x.asFile() for x in TRACKS],
+           suffix(".ini"),
+           ".counts")
+def countWords(infile, outfile):
+    '''count number of words in ini files'''
+
+    # the statement
+    statement = '''awk 'BEGIN { printf("word\\tfreq\\n"); } 
+    {for (i = 1; i <= NF; i++) freq[$i]++}
+    END { for (word in freq) printf "%%s\\t%%d\\n", word, freq[word] }'
+    < %(infile)s > %(outfile)s'''
+    P.run()
 
 
-###################################################################
-# primary targets
-###################################################################
+@transform(countWords,
+           suffix(".counts"),
+           "_counts.load")
+def loadWordCounts(infile, outfile):
+    '''load results of word counting into database.'''
+    P.load(infile, outfile, "--add-index=word")
+
+
+# main target to perform all tasks is the pipeline
+@follows(loadWordCounts)
+def full():
+    pass
 
 
 @follows(mkdir("report"))
