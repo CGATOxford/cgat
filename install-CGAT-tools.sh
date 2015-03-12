bb9e994294e8101b7e61ce6976113b1cfb981f12
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2015-03-12 11:16:49 +0000

    /usr/bin/time is not available on Travis

--- install-CGAT-tools.sh
+++ install-CGAT-tools.sh
@@ -288,7 +288,7 @@ if [ "$OS" == "travis" ] ; then
    export LIBRARY_PATH=$LIBRARY_PATH:"/usr/lib/x86_64-linux-gnu"
 
    # try installing R dependencies to fix errors on Travis
-   conda install r-recommended --yes
+   #conda install r-recommended --yes
    
    # prepare R installation scrip from source
    echo "#!/usr/bin/env R"
@@ -301,8 +301,7 @@ if [ "$OS" == "travis" ] ; then
    echo "install.packages(c(\"WGCNA\",\"flashClust\", \"dtw\"), repos=\"http://mirrors.ebi.ac.uk/CRAN/\")" >> install.R
 
    # and actually install the dependencies
-   /usr/bin/time -o install.R.time -v R CMD BATCH install.R
-   cat install.R.time
+   time R CMD BATCH install.R
    cat install.Rout
 
    cd $TRAVIS_BUILD_DIR
