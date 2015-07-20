5eb8782fb489d58f353ab86b180e1e2e4fb975f7
Andreas Heger <andreas.heger@gmail.com>
2015-07-20 11:22:45 +0100

    removed pandas.com library

--- CGAT/Timeseries/__init__.py
+++ CGAT/Timeseries/__init__.py
@@ -20,7 +20,6 @@ import pandas as pd
 import itertools
 import sys
 import math
-import pandas.rpy.common as com
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.packages import importr
 from rpy2.robjects import r as R
@@ -284,7 +283,7 @@ def deseqNormalize(infile,
                                header=0,
                                sep="\t",
                                compression=comp)
-    rdf = com.convert_to_r_dataframe(data_frame)
+    rdf = pandas2ri.pandas2ri(data_frame)
 
     if not conditions:
         time_rep_comb = [x for x in itertools.product(time_points, reps)]
@@ -343,8 +342,9 @@ def deseqNormalize(infile,
     else:
         R('''trans_vst = data.frame(t(exprs(vst)), times, replicates)''')
 
-    data_file = com.load_data('trans_vst')
-
+    # load data and convert to pandas object
+    data_file = pandas2ri.ri2pandas(R["trans_vst"])
+    
     return data_file
 
 
@@ -410,7 +410,8 @@ def covarFilter(infile,
     df.drop(['times'], inplace=True, axis=1)
     df = df.fillna(0.0)
 
-    R.assign('diff_data', df)
+    # convert data frame and import into R namespace
+    R.assign('diff_data', pandas2ri.pandas2ri(df))
 
     E.info("loading data frame")
 
@@ -440,7 +441,8 @@ def covarFilter(infile,
     R('''filtered_frame <- data.frame(diff_data[, filtered_genes],'''
       '''times, replicates)''')
 
-    filtered_frame = com.load_data('filtered_frame').T
+    # load data and convert to pandas object
+    filtered_frame = pandas2ri.ri2pandas(R["filtered_frame"]).T
 
     return filtered_frame
 
@@ -511,7 +513,7 @@ def clusterPCA(infile,
     R('''eigenPlot(eigen_frame, image.dir="%(image_dir)s", '''
       '''condition="%(header)s")''' % locals())
 
-    eigen_frame = com.load_data("eigen_frame")
+    eigen_frame = pandas2ri.ri2pandas(R["eigen_frame"])
     eigen_frame.index = eigen_frame['cluster']
     eigen_frame.drop(['cluster'], inplace=True, axis=1)
 
@@ -531,7 +533,7 @@ def conditionDESeq2(data_frame, header, alpha, res_dir):
 
     E.info("Differential expression testing for %s" % header)
     cols = data_frame.columns
-    counts = com.convert_to_r_dataframe(data_frame)
+    counts = pandas2ri.pandas2ri(data_frame)
     des_times = ro.IntVector([x.split(".")[1] for x in cols])
     des_reps = ro.StrVector([x.split(".")[2] for x in cols])
     des_cond = ro.StrVector([x.split(".")[0] for x in cols])
@@ -573,7 +575,7 @@ def conditionDESeq2(data_frame, header, alpha, res_dir):
     R('''dev.off()''')
     R('''sink(file=NULL)''')
 
-    df = com.load_data('res.df')
+    df = pandas2ri.ri2pandas(R['res.df'])
 
     return df
 
@@ -586,7 +588,7 @@ def timepointDESeq2(data_frame, header, alpha, res_dir):
 
     E.info("Differential expression testing for %s" % header)
     cols = data_frame.columns
-    counts = com.convert_to_r_dataframe(data_frame)
+    counts = pandas2ri.pandas2ri(data_frame)
     des_times = ro.IntVector([x.split(".")[1] for x in cols])
     des_reps = ro.StrVector([x.split(".")[2] for x in cols])
     genes = ro.StrVector([x for x in data_frame.index])
@@ -625,7 +627,7 @@ def timepointDESeq2(data_frame, header, alpha, res_dir):
     R('''dev.off()''')
     R('''sink(file=NULL)''')
 
-    df = com.load_data('res.df')
+    df = pandas2ri.ri2pandas(R['res.df'])
 
     return df
 
@@ -889,7 +891,7 @@ def maSigPro(infile,
     results_frame.to_csv(results_file, sep="\t")
 
     R('''diff_genes <- data.frame(%(condition)s_fit$SELEC)''' % locals())
-    diff_genes = com.load_data('diff_genes')
+    diff_genes = pandas2ri.ri2pandas[R'diff_genes']
 
     return diff_genes
 
@@ -1202,7 +1204,7 @@ def treeCutting(infile,
     df = df.fillna(0.0)
     genes = df.index
     genes_r = ro.StrVector([g for g in genes])
-    rdf = com.convert_to_r_dataframe(df)
+    rdf = pandas2ri.pandas2ri(df)
     R.assign("distance_data", rdf)
     R.assign("gene_ids", genes_r)
 
@@ -1230,7 +1232,7 @@ def treeCutting(infile,
       '''cluster_matched$cluster)''')
     R('''sink(file=NULL)''')
 
-    cluster_frame = com.load_data('cluster_matched')
+    cluster_frame = pandas2ri.ri2pandas(R["cluster_matched"])
     cluster_frame.columns = ['gene_id', 'cluster']
     cluster_frame.index = cluster_frame['gene_id']
     cluster_frame.drop(['gene_id'], inplace=True, axis=1)
@@ -1366,7 +1368,7 @@ def consensusClustering(infile,
     df = pd.read_table(infile, sep="\t", header=0, index_col=0)
     labels = df.index.tolist()
     labels_r = ro.StrVector([l for l in labels])
-    df_r = com.convert_to_r_dataframe(df)
+    df_r = pandas2ri.pandas2ri(df)
     R.assign("distance.frame", df_r)
     R.assign("labels", labels_r)
 
@@ -1409,6 +1411,6 @@ def consensusClustering(infile,
       '''hang=0.03, main="%(condition)s")''' % locals())
     R('''dev.off()''')
     R('''sink(file=NULL)''')
-    cluster_frame = com.load_data('cluster_matched')
+    cluster_frame = pandas2ri.ri2pandas(R["cluster_matched"])
 
     return cluster_frame
