1f5e90badefc657d7e81d7b5b22e35143bb7fc77
IanSudbery <ian.sudbery@dpag.ox.ac.uk>
2016-07-11 10:12:33 +0100

    Added routines to SRA for remote querying of SRA as well as ENA and GDC

--- CGAT/Sra.py
+++ CGAT/Sra.py
@@ -38,6 +38,8 @@ import os
 import glob
 import tempfile
 import shutil
+import urllib2
+import itertools
 import CGAT.Experiment as E
 import CGAT.Fastq as Fastq
 import CGAT.IOTools as IOTools
@@ -112,7 +114,90 @@ def peek(sra, outdir=None):
 def extract(sra, outdir):
     """return statement for extracting the SRA file in `outdir`."""
 
-    statement = """fastq-dump --split-files --gzip --outdir
+    statement = """fastq-dump --split-files --gzip -Q 33 --outdir
                  %(outdir)s %(sra)s""" % locals()
 
     return statement
+
+
+def prefetch(sra):
+    """Use prefetch from the SRA toolkit to download the local cache"""
+
+    statement = """prefetch %(sra)s -a "$ASCP_BIN_PATH|$ASCP_KEY_PATH" """ % locals()
+    return statement
+
+
+def clean_cache(sra):
+    """Remove the specified SRA file from the cache."""
+
+    statement = """rm `srapath %(sra)s*`""" % locals()
+    return statement
+
+    
+def fetch_ENA(dl_path, outdir, protocol="ascp"):
+    """Fetch fastq from ENA given accession"""
+
+    if protocol == "ascp":
+        statement = """ascp -QT -l %%(aspera_bandwidth)s -i $ASCP_KEY_PATH
+                       era-fasp@fasp.sra.ebi.ac.uk:/%(dl_path)s %(outdir)s """ % locals()
+    elif protocol == "http":
+        fn = os.path.basename(era_path)
+        outfile = os.path.join(outdir, fn)
+        statement = "wget -O %(outfile)s ftp://ftp.sra.ebi.ac.uk:/%(dl_path)s" % locals()
+
+    return statement
+
+
+def fetch_ENA_files(accession):
+    """Get the names of the files matching the ENA accession"""
+    
+    # Get the paths to the files
+    url = "http://www.ebi.ac.uk/ena/data/warehouse/filereport?accession=%s&result=read_run&fields=fastq_ftp" \
+          % accession
+
+    paths = urllib2.urlopen(url).readlines()[1:]
+    paths = list(itertools.chain.from_iterable(
+        [p.strip().split(";") for p in paths]))
+    filenames = [os.path.basename(x) for x in paths]
+    dl_paths = [os.path.join(*x.split("/")[1:]) for x in paths]
+    return filenames, dl_paths
+
+
+def fetch_TCGA_fastq(acc, filename, token=None, outdir="."):
+    """Get Fastq file from TCGA repository. Because of the nature of the
+    TCGA repository it assumes certain things:
+
+            1) That data is paired-end fastq
+            2) That the files end in _1.fastq or _2.fastq
+            """
+
+    statement = []
+
+    if token:
+        token = "-t %s" % token
+    else:
+        token = ""
+
+    statement.append("""gdc-client download
+                         %(token)s
+                         --no-related-files
+                         --no-annotations
+                         -d %(outdir)s
+                         %(acc)s""")
+
+    tar_file = os.path.join(outdir, acc, filename)
+
+    statement.append("tar -xvf %(tar_file)s -C %(outdir)s")
+
+    out_name = os.path.join(outdir, acc)
+
+    statement.append("gzip %(outdir)s/*.fastq")
+    statement.append("rm -r %(out_name)s")
+    statement.append("rename %(outdir)s/*_1.fastq.gz %(outdir)s/%(acc)s_1.fastq.gz %(outdir)s/*")
+    statement.append("rename %(outdir)s/*_2.fastq.gz %(outdir)s/%(acc)s_2.fastq.gz %(outdir)s/*")
+    statement = "; checkpoint;".join(statement)
+
+    return statement % locals()
+
+
+    
