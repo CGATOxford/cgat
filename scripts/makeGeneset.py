63721a0c56c41c7bf11d0f8da81d9229b6523456
katherineb <katherine.brown@dpag.ox.ac.uk>
2016-10-26 16:34:53 +0100

    added more or less than and online gtf option

--- scripts/makeGeneset.py
+++ scripts/makeGeneset.py
@@ -1,3 +1,4 @@
+
 import sys
 import re
 import random
@@ -14,7 +15,35 @@ import CGAT.Pipeline as P
 Filters a GTF to generate a geneset over which to count reads and look for
 differential expression.
 
+Example:
+This would keep only lines from the gtf where gene_biotype is pseudogene or
+protein_coding, contig is not chrM or chrUn and transcript_support is greater
+than 3.
+
+python makeGeneset.py -i refcoding.gtf.gz - o outfile.gtf.gz
+-f "gene_biotype=pseudogene+protein_coding" -f "transcript_support-morethan-3"
+-c "chrM|chrUn"
+
+GTF "columns" are named, in order
+0 contig
+1 source
+2 feature
+3 start
+4 end
+5 ?
+6 strand
+7 frame
+plus anything before an "=" sign in column 8, e.g. gene_source, exon_number
+
 Options
+-g --gtf
+path to local gtf to filter
+
+-p --gtfpath
+path to online gtf to download and filter
+
+-o --outfile
+path to outfile
 
 -c --remove-contigs
 Followed by a list of contigs to remove delimited by |
@@ -28,26 +57,16 @@ e.g. -k chr1|chr12|chr3 only keeps rows of the GTF from chr1, chr12 and chr3
 Any number of filters can be specified as -f or --filter followed by a string
 laid out as follows:
 
-To keep only lines with a specific value in a specified column:
--f COLUMN=VALUE
-e.g.
--f 'source=protein_coding' will return only rows where source = protein_coding
-
-To keep only lines without a specific value in a specified column:
--f COLUMN!=VALUE
-e.g.
--f 'source!=protein_coding' returns only rows where source != protein_coding
-
 To keep only lines with values from a list in a specified column:
--f COLUMN-in-VALUE1+VALUE2+VALUE3
+-f COLUMN=VALUE1+VALUE2+VALUE3
 e.g.
--f 'source-in-protein_coding+lincRNA' will return only rows where source =
+-f 'source=protein_coding+lincRNA' will return only rows where source =
    protein_coding or source = lincRNA
 
 To keep only lines with values not in a list in a specified column
--f COLUMN-notin-VALUE1+VALUE2+VALUE3
+-f COLUMN!=VALUE1+VALUE2+VALUE3
 e.g.
--f 'source-notin-protein_coding+lincRNA' will return only rows where source !=
+-f 'source!=protein_coding+lincRNA' will return only rows where source !=
    protein_coding and source != lincRNA
 
 To keep only lines with values listed in a file
@@ -62,39 +81,57 @@ e.g.
 -f 'source-notin_file-a.tsv' will read the file a.tsv and return only rows of
 the gtf where source is not one of the values in this file
 
+To keep only lines with a value greater than or less than a threshold in
+a specified column
+-f COLUMN-morethan-VALUE or COLUMN-lessthan-VALUE
+e.g. -f "transcript_support-morethan-3" will return only rows with transcript
+support greater than 3
+
 '''
 
 # the old pipeline could also do the following
 # but these were only used by cufflinks - do we still want them?
 # merge exons seperated by small introns
 # ignore very long introns
-
 # remove repetitive RNA - we don't understand what this is?
 
 
+def getGTF(path):
+    statement = "wget %(path)s" % locals()
+    os.system(statement)
+
+
 def filterGTF(gtf, filterstring, tempout):
+
     if "!=" in filterstring:
         column, value = filterstring.split("!=")
-        filtertype = "notequals"
-    elif "=" in filterstring:
-        column, value = filterstring.split("=")
-        filtertype = "equals"
-    elif "-in-" in filterstring:
-        column, value = filterstring.split("-in-")
-        value = value.split("+")
-        filtertype = "in"
-    elif "-notin-" in filterstring:
-        column, value = filterstring.split("-notin-")
         value = value.split("+")
         filtertype = "notin"
+
+    elif "=" in filterstring:
+        column, value = filterstring.split("=")
+        value = value.split("+")
+        filtertype = "in"
+
     elif "-in_file-" in filterstring:
         column, value = filterstring.split("-in_file-")
         value = [line.strip() for line in IOTools.openFile(value)]
         filtertype = "in_file"
+
     elif "-notin_file-" in filterstring:
         column, value = filterstring.split("-notin_file-")
         value = [line.strip() for line in IOTools.openFile(value)]
         filtertype = "notin_file"
+
+    elif "-morethan-" in filterstring:
+        column, value = filterstring.split("-morethan-")
+        value = float(value)
+        filtertype = "morethan"
+
+    elif "-lessthan-" in filterstring:
+        column, value = filterstring.split("-lessthan-")
+        value = float(value)
+        filtertype = "lessthan"
 
     gfile = IOTools.openFile(gtf)
     G = GTF.iterator(gfile)
@@ -110,17 +147,17 @@ def filterGTF(gtf, filterstring, tempout):
         D['strand'] = item.strand
         D['frame'] = item.frame
 
-        if filtertype == "equals":
-            if D[column] == value:
-                out.write("%s\n" % str(item))
-        elif filtertype == "notequals":
-            if D[column] != value:
-                out.write("%s\n" % str(item))
-        elif filtertype == "in" or 'in_file':
+        if filtertype == "in" or filtertype == 'in_file':
             if D[column] in value:
                 out.write("%s\n" % str(item))
-        elif filtertype == "notin" or 'notin_file':
+        elif filtertype == "notin" or filtertype == 'notin_file':
             if D[column] not in value:
+                out.write("%s\n" % str(item))
+        elif filtertype == "morethan":
+            if float(D[column]) > value:
+                out.write("%s\n" % str(item))
+        elif filtertype == "lessthan":
+            if float(D[column]) < value:
                 out.write("%s\n" % str(item))
     out.close()
     gfile.close()
@@ -145,6 +182,10 @@ def main(argv=None):
     parser.add_option("-g", "--gtf", dest="gtf",
                       type="string",
                       help="path to input gtf")
+
+    parser.add_option("-p", "--gtfpath", dest="gtfpath",
+                      type="string",
+                      help="path to online gtf")
 
     parser.add_option("-c", "--remove-contigs", dest="remove_contigs",
                       type="string",
@@ -171,16 +212,27 @@ def main(argv=None):
 
     (options, args) = E.Start(parser)
 
-    gtf = options.gtf
+    if options.gtf:
+        gtf = options.gtf
+    elif options.gtfpath:
+        getGTF(options.gtfpath)
+        gtf = options.gtfpath.split("/")[-1]
+    else:
+        raise ValueError("Please provide a GTF or the path to an online GTF")
 
+    if not options.outfile:
+        raise ValueError("Please provide an output file name")
+
+    d = 0
     if options.remove_contigs or options.keep_contigs:
+        d += 1
         statement = 'zcat %s |' % gtf
 
         if options.remove_contigs:
             statement += removeNamedContigs(options.remove_contigs)
 
         if options.keep_contigs:
-             statement += keepOnlyNamedContigs(options.keep_contigs)
+            statement += keepOnlyNamedContigs(options.keep_contigs)
 
         if options.outfile.endswith(".gz"):
             outfile = options.outfile
@@ -192,13 +244,19 @@ def main(argv=None):
         os.system(statement)
 
     T1 = gtf
-    for filterstring in options.filters:
-        T2 = P.getTempFilename(".")
-        T2 = T2 + ".gtf"
-        filterGTF(T1, filterstring, T2)
-        T1 = T2
+    if options.filters:
+        d += 1
+        for filterstring in options.filters:
+            T2 = P.getTempFilename(".")
+            T2 = T2 + ".gtf"
+            filterGTF(T1, filterstring, T2)
+            T1 = T2
 
-    shutil.move(T2, options.outfile)
+        shutil.move(T2, options.outfile)
+
+    if d == 0:
+        raise ValueError("No filters provided")
+
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
