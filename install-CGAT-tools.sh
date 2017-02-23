b7643a26b0e60d93decbb2d8244ea94e53fb80f1
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2017-02-23 10:28:56 +0000

    make sure TEST_ALL gathers aggregate result

--- install-CGAT-tools.sh
+++ install-CGAT-tools.sh
@@ -403,9 +403,9 @@ if [ $TRAVIS_INSTALL ] || [ $JENKINS_INSTALL ] ; then
    # run nosetests
    if [ $TEST_ALL ] ; then
       log "test_import.py" && nosetests -v tests/test_import.py && \
-      log "test_style.py" nosetests -v tests/test_style.py && \
-      log "test_commandline" nosetests -v tests/test_commandline.py && \
-      log "test_scripts" nosetests -v tests/test_scripts.py ;
+      log "test_style.py" && nosetests -v tests/test_style.py && \
+      log "test_commandline" && nosetests -v tests/test_commandline.py && \
+      log "test_scripts" && nosetests -v tests/test_scripts.py ;
    elif [ $TEST_IMPORT ] ; then
       nosetests -v tests/test_import.py ;
    elif [ $TEST_STYLE ] ; then
