d706a14c19f30eaad0513e6ec73ab0edcf7c3a9f
Andreas Heger <andreas.heger@gmail.com>
2017-05-23 16:43:16 +0100

    {AH} py3 fixes

--- tests/GTF_test.py
+++ tests/GTF_test.py
@@ -4,15 +4,16 @@ import os
 import CGAT.GTF as GTF
 import CGAT.IOTools as IOTools
 
+
 class TestIteration(unittest.TestCase):
 
     filename = os.path.join("data", "hg19.small.gtf.gz")
 
     def test_number_of_intervals_is_correct(self):
-        
+
         with IOTools.openFile(self.filename) as inf:
             records = list(GTF.iterator(inf))
-        
+
         self.assertEqual(len(records),
                          100)
 
