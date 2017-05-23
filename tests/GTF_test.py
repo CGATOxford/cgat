a2d0f2fe5ffefcc378a617ed601b1a603e5b2c75
Andreas Heger <andreas.heger@gmail.com>
2017-05-23 13:46:11 +0100

    {AH} add GTF_test

---
+++ tests/GTF_test.py
@@ -0,0 +1,21 @@
+import unittest
+import os
+
+import CGAT.GTF as GTF
+import CGAT.IOTools as IOTools
+
+class TestIteration(unittest.TestCase):
+
+    filename = os.path.join("data", "hg19.small.gtf.gz")
+
+    def test_number_of_intervals_is_correct(self):
+        
+        with IOTools.openFile(self.filename) as inf:
+            records = list(GTF.iterator(inf))
+        
+        self.assertEqual(len(records),
+                         100)
+
+
+if __name__ == "__main__":
+    unittest.main()
