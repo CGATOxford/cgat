4b822410e4ab5a79af840094ccbaee79b925edc4
Sebastian Luna-Valero <sebastian-luna-valero@users.noreply.github.com>
2017-08-29 10:45:42 +0100

    Update install-CGAT-tools.sh
    Make sure TRAVIS_BRANCH is initialised.

--- install-CGAT-tools.sh
+++ install-CGAT-tools.sh
@@ -192,8 +192,7 @@ log "installing CGAT environment"
 if [[ $INSTALL_SCRIPTS ]] ; then
    conda create -q -n ${CONDA_INSTALL_ENV} cgat-scripts --override-channels -c bioconda -c conda-forge -c defaults -y
 else
-   # to-update once we merge to master:
-   # wget -O env.yml https://raw.githubusercontent.com/CGATOxford/cgat/master/conda/environments/${CONDA_INSTALL_TYPE}
+   [[ -z ${TRAVIS_BRANCH} ]] && TRAVIS_BRANCH=${INSTALL_BRANCH}
    wget -O env.yml https://raw.githubusercontent.com/CGATOxford/cgat/${TRAVIS_BRANCH}/conda/environments/${CONDA_INSTALL_TYPE}
    conda env create -f env.yml
 fi
